473af1d2a0bb006032aa4e73a7a1d07e11a3422c
Caio Emanuel Silva <caio.silva203@fatec.sp.gov.br>
2024-07-31 20:09:26 -0300

    first commit

---
+++ Scripts/script.js
@@ -0,0 +1,54 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const inputText = document.getElementById('input-text');
+    const outputText = document.getElementById('output-text');
+    const placeholderImg = document.getElementById('placeholder-img');
+    const outputPlaceholder = document.getElementById('output-text-placeholder');
+    const encryptButton = document.getElementById('encrypt-btn');
+    const decryptButton = document.getElementById('decrypt-btn');
+    const copyButton = document.getElementById('copy-btn');
+
+    encryptButton.addEventListener('click', () => {
+        const encryptedText = encryptText(inputText.value);
+        displayResult(encryptedText);
+    });
+
+    decryptButton.addEventListener('click', () => {
+        const decryptedText = decryptText(inputText.value);
+        displayResult(decryptedText);
+    });
+
+    copyButton.addEventListener('click', () => {
+        outputText.select();
+        document.execCommand('copy', false);
+    });
+
+    function displayResult(text) {
+        placeholderImg.style.display = 'none';
+        outputPlaceholder.style.display = 'none';
+        outputText.style.display = 'block';
+        outputText.value = text;
+        copyButton.style.display = 'block';
+    }
+
+    function encryptText(text) {
+        const map = {
+            'e': 'enter',
+            'i': 'imes',
+            'a': 'ai',
+            'o': 'ober',
+            'u': 'ufat'
+        };
+        return text.replace(/[eioua]/g, match => map[match]);
+    }
+
+    function decryptText(text) {
+        const map = {
+            'enter': 'e',
+            'imes': 'i',
+            'ai': 'a',
+            'ober': 'o',
+            'ufat': 'u'
+        };
+        return text.replace(/enter|imes|ai|ober|ufat/g, match => map[match]);
+    }
+});
